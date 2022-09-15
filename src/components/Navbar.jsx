354b4231321460a1f24deff4cc30da64ee11cb37
Lambertn33 <lambertgithub@gmail.com>
2022-09-15 13:30:03 +0200

    feat:initial commit and bootstrap installation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Companies List</a>
+        </div>
+    </nav>
+  )
+}
